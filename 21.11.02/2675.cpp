7aa9840cae7ffc2e91d2db8da52216ff7ec19bd7
Jeonhyunjun <hyunjun050915@gmail.com>
2021-11-02 22:01:39 +0900

    주석추가 완료

--- 21.11.02/2675.cpp
+++ 21.11.02/2675.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 int main()
 {
-	int T, R;
+	int T, R; 
 	string S;
 	cin >> T;
 	
-	for(int i = 0; i < T; i++)
+	for(int i = 0; i < T; i++) // 입력받은 T보다 커지거나 같을때까지 반복 
 	{
 		
 		cin >> R >> S;
-		for(int k = 0; k < S.length(); k++)
+		for(int k = 0; k < S.length(); k++) // k는 S의 크기만큼 반복 
 		{
-			for(int j = 0; j < R; j++)
-				cout << S[k];
+			for(int j = 0; j < R; j++) //위와 동일하게 R만큼(R의 크기X) 
+				cout << S[k]; 
 		}
 		cout << endl;
 	}
