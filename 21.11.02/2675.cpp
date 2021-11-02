eb3bcd7605622e7cd600e39aaa8c1e1bb6c060d9
Jeonhyunjun <jhj091542@gmail.com>
2021-11-02 21:50:07 +0900

    구글링으로 맞춘거라 , 주석으로 해석 필요합니당 :)

--- 21.11.02/2675.cpp
+++ 21.11.02/2675.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main()
 {
-	string S = 0; // S를 R번 반복하고  
-	int T,R;
-	
-	cin >> S >> R;
+	int T, R;
+	string S;
+	cin >> T;
 	
 	for(int i = 0; i < T; i++)
 	{
+		
+		cin >> R >> S;
 		for(int k = 0; k < S.length(); k++)
 		{
 			for(int j = 0; j < R; j++)
-			{
 				cout << S[k];
-			}
 		}
 		cout << endl;
 	}
-	
-	return 0;
+	return 0;	
 }
