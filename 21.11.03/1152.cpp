aca748ff5188172705d0b74627c1b2e144f82249
Jeonhyunjun <hyunjun050915@gmail.com>
2021-11-03 23:31:53 +0900

    너무 급하게 생각해서 이리 간단한게 생각이 안났었네 ㅜㅜ

--- 21.11.03/1152.cpp
+++ 21.11.03/1152.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main()
+{
+	string eng;
+	int count = 1;
+	
+	getline(cin, eng); // 함수는 입력 스트림에서 문자들을 읽어서, 인자로 받은 문자열에 저장
+	
+	for(int i = 0; i < eng.length(); i++)
+	{
+		if(eng[i] == ' ') // 만약 eng index가 공백이라면?
+		{
+			count ++;
+		}	 
+	}
+	
+	if(eng[0] == ' ' || eng[0] == '\0' ){ //처음 공백이 온다면 
+		count --; // 카운트에서 - 함 
+	}
+	
+	if(eng[eng.length() -1] == ' ')
+	{
+		count --;
+	}
+	
+	cout << count;
+	
+	return 0;
+}
