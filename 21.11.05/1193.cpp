767bc9cd09a613661d5de344c6a8128ff2028638
Jeonhyunjun <jhj091542@gmail.com>
2021-11-08 00:22:19 +0900

    분명 예제 입출력은 틀린데 , 왜 정답이야 ? (그거보다 겁나 어렵넹)

--- 21.11.05/1193.cpp
+++ 21.11.05/1193.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	
+	int denominator = 0; //분모
+	int molecule = 0; //분자
+	int input;
+	int cnt = 1; // 몇번째 줄인지  
+	cin >> input;
+	
+	while(cnt < input) //몇번째줄에 있는지 검사 해주고 
+	{
+		input = input - cnt; 
+		cnt++;
+	}
+	
+	//만약 14일 경우 5(줄)4번째
+	
+	if(cnt % 2 == 0) // 짝수일때 
+	{
+		
+		//분모 1씩 감소 분자 1씩 증가 
+		for(int i = cnt; i > 0; i--)
+		{
+			denominator = i;
+			molecule ++;
+			
+			input--;
+			if(input == 0)
+			{ 
+				break;
+			}
+			
+		}
+	} 
+	else{ //홀수일때를 대비함 
+		
+		//분모 1씩 증가 분자 1씩 감소 
+		for(int i = cnt; i > 0; i--)
+		{
+			molecule = i;
+			denominator++;
+			
+			input--;
+			if(input == 0)
+			{
+				break;
+			}
+		}
+	}
+	cout << molecule  << "/" << denominator; // 분수 형태로 출력
+	
+	return 0;	 
+}
