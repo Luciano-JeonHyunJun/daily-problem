30501edead4f20fee0e6d04ef846cdfcb23a49b7
Jeonhyunjun <jhj091542@gmail.com>
2021-11-07 15:58:50 +0900

    뭔가 어려운데 쉬운 느낌이랄까..

--- 21.11.04/1712.cpp
+++ 21.11.04/1712.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int a , b , c; //고정금액 , 가변금액 , 노트북 가격
+	cin >> a >> b >> c;
+	
+	int total = a + b; // 총 금액 
+	int bep; // 손익 분기점 
+	
+	if(b >=c)
+		{
+			cout << "-1";
+			return 0;
+		}
+		
+		cout << a/(c-b) + 1;
+		
+	}
