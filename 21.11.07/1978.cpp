cfab400711978592e185d696fa74f1534482b5ba
Jeonhyunjun <jhj091542@gmail.com>
2021-11-09 17:55:39 +0900

    절대 제출 X

--- 21.11.07/1978.cpp
+++ 21.11.07/1978.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+int main() {
+	int count = 0, max, num, check = 0;
+	cin >> max;
+
+	while (max--) {
+		cin >> num;
+
+		if (num == 1)
+			continue;
+
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				check++;
+				break;
+			}
+		}
+		if (!check)
+			count++;
+		check = 0;
+	}
+	cout << count << endl;
+}
