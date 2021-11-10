a8a32562eccc52deb079ef5dee6a53b7c7a7063f
Jeonhyunjun <jhj091542@gmail.com>
2021-11-10 14:37:53 +0900

    풀었긴한데 너무 어려워서 설명 불가능 ...

--- 21.11.06/10250.cpp
+++ 21.11.06/10250.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-    int T;
-    cin>>T;
-    int H,W,N;
-    for(int i=0; i<T; i++){
-        cin>>H>>W>>N;
-        int H1, W1;
-        H1 = N%H;
-        W1 = N/H;
-        if(H1 > 0)
-            W1++;
-        else
-            H1 = H;
-        cout<<H1 * 100 + W1<<'\n';
-    }
+int main()
+{
+	int t;
+	cin >> t;
+	
+	
+	int h , w, n;
+	
+	for(int i = 0; i < t; i++)
+	{
+		cin >> h , w , n;
+		
+		int H1 , W1;
+		
+		H1 = n%h;
+		W1 = n/h;
+		
+		if(H1 > 0)
+			W1++;
+		else
+			H1 = h;
+			cout << H1 * 100 + W1 << '\n';
+	}	
 }
